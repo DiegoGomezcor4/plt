00e65e83414ce8bf57c6d7e490d57e9e0f8ebb4f
Diego Gomez <diegocor4@gmail.com>
2024-11-07 18:14:17 -0300

    do while

---
+++ do_while.js
@@ -0,0 +1,14 @@
+/*
+do {
+} while (condicion)
+
+verifica la condicion despues de ejecutar el codigo
+
+*/
+
+let contador = 0;
+do {
+    console.log(contador);
+
+    contador++;
+} while(contador < 10);
